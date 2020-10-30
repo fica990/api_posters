346d4e1623f3af00cd54b27a90ac261174412af7
filip <filipmitrovic90@gmail.com>
2020-10-30 20:17:10 +0100

    added seeder for images table

---
+++ src/database/seeds/ImageSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('images')->insert([
+            'name' => 'plane;jpg',
+            'path' => '/images/planes/',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
